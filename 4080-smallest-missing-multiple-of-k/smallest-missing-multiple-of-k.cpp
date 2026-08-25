8bf103d69e3414e857b059fae789c06ff79b9ec7
chaitanyakolekar <chaitanyakolekar2649@gmail.com>
2026-08-26 00:12:13 +0530

    Time: 3 ms (38.06%) | Memory: 25.1 MB (45.06%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-       set<int>st;
+       unordered_set<int>st;
        int mx=0;
        for(auto &i:nums){
         st.insert(i);
         mx=max(i,mx);
        }
         int ans=0;
-       for(int i=1;i<=mx+1;i++){
+       for(int i=1;i<1000;i++){
         if(st.find(i*k)==st.end()){
                 ans=i*k;
                 break;
