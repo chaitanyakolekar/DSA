55d4e7fa7556363740fe4be2062e0e0b7c99292b
chaitanyakolekar <chaitanyakolekar2649@gmail.com>
2026-08-26 15:06:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (12.45%) - LeetSync

---
+++ 27-remove-element/remove-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int count=0;
+        int n=nums.size();
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==val){
+                count++;
+                nums.erase(nums.begin()+i);
+                i--;
+            }
+        }
+        return n-count;
+    }
+};
